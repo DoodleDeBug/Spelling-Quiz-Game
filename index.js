a507771d8fb12368233598a87dc972ffd5120e97
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-06-22 22:56:47 +0100

    can add new spellings to spellings array

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-// const spelling = (() {
+// const spelling = (() { //initialize iffe
+
+//variables
+
+let week = 1;
 let spellings = [
   {
     1: "Dog",
@@ -10,7 +14,7 @@ let spellings = [
     7: "Button",
     8: "Button",
     9: "Button",
-    10: "Button",
+    10: "Carrot",
   },
   {
     1: "Dog",
@@ -22,20 +26,42 @@ let spellings = [
     7: "Button",
     8: "Button",
     9: "Button",
-    10: "Button",
+    10: "Whole",
   },
 ];
 
 // cache DOM
 const container = document.getElementById("container");
 const form = document.querySelector("form");
+const spellingList = document.querySelectorAll("input");
 
 //add event listeners
+form.addEventListener("submit", addSpellings);
 
-form.addEventListener("submit", thing);
+function render() {}
 
-function thing(e) {
-  e.preventDefault();
-  console.log("things");
+function addSpellings(e) {
+  e.preventDefault(); // prevent refresh of page on form submit
+
+  let i = 1; // start week counter off
+
+  window["week" + week] = {}; // empty object for variable week name
+
+  spellingList.forEach((spelling) => {
+    window["week" + week][i] = spelling.value;
+    i++;
+  });
+
+  console.log(window["week" + week]);
+  spellings.push(window["week" + week]); //push week object to spelling array
+
+  console.log(spellings);
+  clearForm();
+  week++;
 }
+
+function clearForm() {
+  spellingList.forEach((spelling) => (spelling.value = ""));
+}
+
 // })
