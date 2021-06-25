d00f76f2b3b80ba89b184fd27feb33c1b9e18c14
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-06-25 11:50:21 +0100

    enable toggle modal

--- index.js
+++ index.js
@@ -2,32 +2,6 @@
 
 //variables
 let week = 1;
-// let spellings = [
-//   {
-//     1: "Dog",
-//     2: "Button",
-//     3: "Button",
-//     4: "Button",
-//     5: "Button",
-//     6: "Button",
-//     7: "Button",
-//     8: "Button",
-//     9: "Button",
-//     10: "Carrot",
-//   },
-//   {
-//     1: "Dog",
-//     2: "Button",
-//     3: "Button",
-//     4: "Button",
-//     5: "Button",
-//     6: "Button",
-//     7: "Button",
-//     8: "Button",
-//     9: "Button",
-//     10: "Whole",
-//   },
-// ];
 
 let spellings = [
   [
@@ -59,7 +33,7 @@ let spellings = [
 // cache DOM
 const container = document.getElementById("container");
 const form = document.querySelector("form");
-const spellingList = document.querySelectorAll("input");
+const spellingInput = document.querySelectorAll("input");
 
 //add event listeners
 form.addEventListener("submit", addSpellings);
@@ -118,13 +92,13 @@ function render() {
 function addSpellings(e) {
   e.preventDefault(); // prevent refresh of page on form submit
 
-  let i = 1; // start week counter off
+  // let i = 1; // start week counter off
 
-  window["week" + week] = {}; // empty object for variable week name
+  window["week" + week] = []; // empty object for variable week name
 
-  spellingList.forEach((spelling) => {
-    window["week" + week][i] = spelling.value;
-    i++;
+  spellingInput.forEach((spelling) => {
+    window["week" + week].push = spelling.value;
+    // i++;
   });
 
   console.log(window["week" + week]);
@@ -136,7 +110,7 @@ function addSpellings(e) {
 }
 
 function clearForm() {
-  spellingList.forEach((spelling) => (spelling.value = ""));
+  spellingInput.forEach((spelling) => (spelling.value = ""));
 }
 
 // })
