8149500f6d4f0cf0a8f1bf37146cc288ce9be978
M <75613073+DoodleDeBug@users.noreply.github.com>
2021-07-11 17:36:41 +0100

    turn is functional, with spelling sound

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
 const test = (() => {
   // cache DOM
   const speakerBtn = document.querySelector(".speaker");
-  const answerField = document.getElementById("answer");
+  const form = document.querySelector("#form");
+  const answerField = document.querySelector("#answer");
   const weekDisplay = document.querySelector(".week");
   const qNumDisplay = document.querySelector(".qNum");
 
@@ -9,17 +10,59 @@ const test = (() => {
   let weekNum = localStorage.getItem("currentTestWeek");
   let spellings = localStorage.getItem("currentTestSpellings").split(",");
 
-  console.log(weekNum);
+  //format spellings
+  let formattedSpellings = [];
+  spellings.forEach((word) => formattedSpellings.push(word.toLowerCase()));
+
+  //   console.log(weekNum);
   console.log(spellings);
+  console.log(formattedSpellings);
 
   //variables
-  let qNum = 1;
+  let qNum = 0;
+  let answers = [];
 
   // add event listeners
-  //   speakerBtn.addEventListener("click", speak);
-  //   answerField.addEventListener("submit", processAnswer);
+  form.addEventListener("submit", newTurn);
 
   // display
   weekDisplay.innerText = `Week ${weekNum}`;
-  qNumDisplay.innerText = `Q${qNum}.`;
+
+  newTurn();
+
+  ////FUNCTIONS////////////////////////////////////////////////////////////
+
+  function newTurn(e) {
+    if (e) {
+      e.preventDefault();
+    }
+
+    if (qNum == 10) {
+      alert("game over");
+    }
+
+    answers.push(answerField.value.toLowerCase());
+    answerField.value = "";
+    console.log(answers.slice(1));
+
+    qNum++;
+    qNumDisplay.innerText = `Q${qNum}.`;
+    console.log(`turn ${qNum}`);
+
+    // add event listeners
+    speakerBtn.addEventListener("click", speak);
+  }
+
+  function speak() {
+    toggleSound();
+    setTimeout(toggleSound, 1000);
+
+    let spelling = new SpeechSynthesisUtterance();
+    spelling.text = spellings[qNum - 1];
+    window.speechSynthesis.speak(spelling);
+  }
+
+  function toggleSound() {
+    speakerBtn.firstChild.classList.toggle("bi-volume-up-fill");
+  }
 })();
